834bd2db307831826b6dd3031951297511f3ac71
narczi <smiechu47@gmail.com>
2023-02-15 12:36:36 +0100

    nowy porządek

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,16 @@ from django.urls import path, include
 
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
